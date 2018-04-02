75bf7288311090bdf27ed5c2d554589b9d935d61
JunminPark <jun.m.park0@gmail.com>
2018-04-02 19:32:52 +0900

    2018-04-2 07:32PM test1

--- app/src/main/java/com/example/volunteer_matching/MainActivity.java
+++ app/src/main/java/com/example/volunteer_matching/MainActivity.java
@@ -10,8 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //hellu~~~~~I'm kung kwang~~
-
+        //dsdaf
 
     }
 }
