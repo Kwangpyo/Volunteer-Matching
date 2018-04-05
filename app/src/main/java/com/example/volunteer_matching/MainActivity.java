1a2832a51d07a738019fcbb18c50f52b7299301e
JunminPark <jun.m.park0@gmail.com>
2018-04-05 20:44:57 +0900

    2018-04-05 08:44PM main page connect to login page

--- app/src/main/java/com/example/volunteer_matching/MainActivity.java
+++ app/src/main/java/com/example/volunteer_matching/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.volunteer_matching;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
         Button btn_yes = (Button) findViewById(R.id.btn_yes);
         Button btn_no = (Button) findViewById(R.id.btn_no);
 
+        btn_no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toLogin = new Intent(MainActivity.this, LoginActivity.class);
+
+                startActivity(toLogin);
+                finish();
+            }
+        });
 
     }
 }
