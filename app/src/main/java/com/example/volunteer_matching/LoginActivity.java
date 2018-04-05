d5b6fb40e22ff4626360ca33b2e4b7b8859a3304
JSY <soyoung6488@ajou.ac.kr>
2018-04-05 20:54:39 +0900

    2018-04-05 08:54 PM change login activity(1)

--- app/src/main/java/com/example/volunteer_matching/LoginActivity.java
+++ app/src/main/java/com/example/volunteer_matching/LoginActivity.java
@@ -1,13 +1,88 @@
 package com.example.volunteer_matching;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText idInput, passwordInput;
+    CheckBox autoLogin;
+    Boolean loginChecked;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        idInput = (EditText) findViewById(R.id.emailInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+        autoLogin = (CheckBox) findViewById(R.id.checkBox);
+
+        // if autoLogin checked, get input
+        if (pref.getBoolean("autoLogin", false)) {
+            idInput.setText(pref.getString("id", ""));
+            passwordInput.setText(pref.getString("pw", ""));
+            autoLogin.setChecked(true);
+            // goto mainActivity
+
+        } else {
+            // if autoLogin unChecked
+            String id = idInput.getText().toString();
+            String password = passwordInput.getText().toString();
+            Boolean validation = loginValidation(id, password);
+
+            if(validation) {
+                Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_LONG).show();
+                // save id, password to Database
+
+                if(loginChecked) {
+                    // if autoLogin Checked, save values
+                    editor.putString("id", id);
+                    editor.putString("pw", password);
+                    editor.putBoolean("autoLogin", true);
+                    editor.commit();
+                }
+                // goto mainActivity
+
+            } else {
+                Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+                // goto LoginActivity
+            }
+        }
+
+        // set checkBoxListener
+        autoLogin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    loginChecked = true;
+                } else {
+                    // if unChecked, removeAll
+                    loginChecked = false;
+                    editor.clear();
+                    editor.commit();
+                }
+            }
+        });
     }
+
+    private boolean loginValidation(String id, String password) {
+        if(pref.getString("id","").equals(id) && pref.getString("pw","").equals(password)) {
+            // login success
+            return true;
+        } else if (pref.getString("id","").equals(null)){
+            // sign in first
+            Toast.makeText(LoginActivity.this, "Please Sign in first", Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+            // login failed
+            return false;
+        }
+    }
+
 }
