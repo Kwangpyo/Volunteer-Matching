629f261f9a662b4c48fda0b9103780ad3bad46b1
Ko Kwangpyo <kwangpyo@naver.com>
2018-04-08 20:56:02 +0900

    20180408-고광표-스키마

--- Web/volunteer-matching/server/routes/main.js
+++ Web/volunteer-matching/server/routes/main.js
@@ -31,18 +31,28 @@ router.post('/testPOST',function(req,res){
 
 //스키마 만들기
 var managerSchema = new mongoose.Schema({
-  //광표야 여기
-  /*ex)  title : String,
-  content : String*/
+  userID: String,
+  userPWD: String,
+  userNAME: String,
+  userPhoneNUM: [Number],
+  ID_NUM: [Number],
+  userJOB: String,
+  userADDR: String,
+  userEmail_before: String,
+  userEmail_after: String,
+  userVolCareer: String,
+  userFeedbackVal:Number
 });
 //모델 만들기
 var Manager = mongoose.model('Manager',managerSchema);
 // POST 요청
 router.post('/requestSignUp',function (req,res) {
   Manager.findOne({},function (err,info) {
-    
+
+
+
   })
-  
+
 })
 
 module.exports = router;
